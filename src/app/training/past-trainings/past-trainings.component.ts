2091d458243075c96389cdcf686e6f387080f8ce
kennethsilvere <kennethsilvere93@gmail.com>
2019-05-03 17:17:52 -0400

    Implement filter and paginator for past-trainings table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatSort } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
@@ -9,19 +9,28 @@ import { Exercise } from '../exercise.model';
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
 
   dataSource = new MatTableDataSource<Exercise>();
 
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getPastExercises();
+  }
+
+  ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  filterTable(filterKeyword: string) {
+    this.dataSource.filter = filterKeyword.trim().toLowerCase();
   }
 
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -25,7 +25,6 @@ export class TrainingService {
       return ex.id === selectedExerciseId;
     });
     this.exerciseChanged.next(this.currentExercise);
-    console.log(this.currentExercise);
   }
 
   getRunningExercise() {
